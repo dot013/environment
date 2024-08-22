3bc615614aee453f9fc0b135fc88eef0b36f6d40
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-08-22 16:03:48 -0300

    fix: remove json5 support

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -3,17 +3,16 @@ return {
 		"mfussenegger/nvim-dap",
 		dependencies = {
 			"leoluz/nvim-dap-go",
-			{
+			--[[ {
 				"Joakker/lua-json5",
-				build = "./install.sh",
-			},
+			}, ]]
 		},
 		lazy = false,
 		config = function()
 			local dap = require("dap")
 			local dapgo = require("dap-go")
 
-			require("dap.ext.vscode").json_decode = require("json5").parse
+			-- require("dap.ext.vscode").json_decode = require("json5").parse
 
 			dap.setup()
 			dapgo.setup()
@@ -25,13 +24,13 @@ return {
 				vim.keymap.set("n", key, func, { desc = desc })
 			end
 
-			nmap("<leader>dj", require("dap.ext.vscode").load_launchjs())
+			nmap("<leader>xj", require("dap.ext.vscode").load_launchjs())
 
-			nmap("<leader>dp", dap.toggle_breakpoint())
-			nmap("<leader>dc", dap.continue())
-			nmap("<leader>do", dap.step_over())
-			nmap("<leader>di", dap.step_into())
-			nmap("<leader>dO", dap.repl.open())
+			nmap("<leader>xp", dap.toggle_breakpoint())
+			nmap("<leader>xc", dap.continue())
+			nmap("<leader>xo", dap.step_over())
+			nmap("<leader>xi", dap.step_into())
+			nmap("<leader>xO", dap.repl.open())
 		end,
 	},
 }
