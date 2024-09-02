9237ede9ef59a01498671a31896bf6aeba190a1c
Gustavo "Guz" L. de Mello <contact@guz.one>
2024-09-02 11:28:33 -0300

    feat: mosh

--- configuration.nix
+++ configuration.nix
@@ -1,33 +1,32 @@
-{ config
-, pkgs
-, lib
-, ...
-}:
-let
-  cfg = config.dot013.environment;
-in
 {
-  imports = [ ];
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  cfg = config.dot013.environment;
+in {
+  imports = [];
   options.dot013.environment = with lib;
-    with lib.types; {
-      enable = mkEnableOption "";
-      interception-tools.enable = mkOption {
-        type = bool;
-        default = true;
-      };
-      interception-tools.device = mkOption {
-        type = str;
-      };
+  with lib.types; {
+    enable = mkEnableOption "";
+    interception-tools.enable = mkOption {
+      type = bool;
+      default = true;
     };
+    interception-tools.device = mkOption {
+      type = str;
+    };
+    mosh.enable = mkEnableOption "";
+  };
   config = with lib;
     mkIf cfg.enable {
-      services.interception-tools =
-        let
-          device = cfg.interception-tools.device;
-        in
+      services.interception-tools = let
+        device = cfg.interception-tools.device;
+      in
         mkIf cfg.interception-tools.enable {
           enable = true;
-          plugins = [ pkgs.interception-tools-plugins.caps2esc ];
+          plugins = [pkgs.interception-tools-plugins.caps2esc];
           udevmonConfig = ''
             - JOB: "${pkgs.interception-tools}/bin/intercept -g ${device} | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc -m 2 | ${pkgs.interception-tools}/bin/uinput -d ${device}"
               DEVICE:
@@ -37,11 +36,15 @@ in
           '';
         };
 
+      programs.mosh = mkIf cfg.mosh.enable {
+        enable = true;
+      };
+
       environment.sessionVariables = {
         EDITOR = mkDefault "nvim";
       };
 
-      environment.pathsToLink = [ " /share/zsh " ];
+      environment.pathsToLink = [" /share/zsh "];
 
       programs.zsh.enable = mkDefault true;
 
@@ -51,7 +54,7 @@ in
       fonts.fontconfig.enable = mkDefault true;
       fonts.packages = with pkgs; [
         fira-code
-        (nerdfonts.override { fonts = [ "FiraCode" ]; })
+        (nerdfonts.override {fonts = ["FiraCode"];})
       ];
     };
 }
