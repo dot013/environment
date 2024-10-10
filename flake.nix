b830e10198fe95353d6bb3111f6557f0620ac1ec
Gustavo L de Mello (Guz) <contact@guz.one>
2024-10-09 22:31:03 -0300

    chore: update lock files

--- flake.nix
+++ flake.nix
@@ -3,40 +3,36 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs =
-    { self
-    , nixpkgs
-    ,
-    }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        f system pkgs);
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: let
+      pkgs = import nixpkgs {inherit system;};
     in
-    {
-      nixosModules = {
-        dot013-environment = import ./configuration.nix;
-        default = self.nixosModules.dot013-environment;
-      };
-      homeManagerModules = {
-        dot013-environment = import ./home.nix;
-        default = self.homeManagerModules.dot013-environment;
-      };
-      homeManagerModule = self.homeManagerModules.dot013-environment;
-      devShells = forAllSystems (system: pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            stylua
-          ];
-        };
-      });
+      f system pkgs);
+  in {
+    nixosModules = {
+      dot013-environment = import ./configuration.nix;
+      default = self.nixosModules.dot013-environment;
     };
+    homeManagerModules = {
+      dot013-environment = import ./home.nix;
+      default = self.homeManagerModules.dot013-environment;
+    };
+    homeManagerModule = self.homeManagerModules.dot013-environment;
+    devShells = forAllSystems (system: pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          stylua
+        ];
+      };
+    });
+  };
 }
